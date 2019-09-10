b7e54a698343ff21a96ac1082d20d996490f4eb9
yaxlie <m.lesnierowski@gmail.com>
2019-09-10 22:06:33 +0200

    Add ocr.py file

--- main.py
+++ main.py
@@ -1,47 +1,8 @@
 import sys
-
-import cv2
-import yaml
-
-import processing
-
-DEBUG = False
-
-rec_numbers = processing.RecognizeNumbers()
-
-
-def ocr(path_to_image):
-    result = []
-
-    with open("params.yaml", 'r') as stream:
-        params = yaml.safe_load(stream)
-
-    image = cv2.imread(path_to_image, cv2.IMREAD_COLOR)
-
-    pp_image = processing.PreProcessing(image, params['gamma'], params['contrast'], params['threshold']).result_image
-    lines_util = processing.LinesUtil(image, pp_image)
-
-    lines = lines_util.get_lines()
-
-    for line in lines:
-        # Name recognition
-        name = None
-        # todo
-
-
-        # Surname recognition
-        surname = None
-        # todo
-
-        index = rec_numbers.get_index(image, line)
-
-        result.append((name, surname, index))
-
-    return result
-
+import ocr
 
 if len(sys.argv) > 1:
-    result = ocr(str(sys.argv[1]))
+    result = ocr.ocr(str(sys.argv[1]))
     print('\n'.join(map(str, result)))
 else:
     print("Podaj ścieżkę do zdjęcia.")
---
+++ ocr.py
@@ -0,0 +1,34 @@
+import cv2
+import yaml
+import processing
+
+
+def ocr(path_to_image):
+    rec_numbers = processing.RecognizeNumbers()
+
+    result = []
+
+    with open("params.yaml", 'r') as stream:
+        params = yaml.safe_load(stream)
+
+    image = cv2.imread(path_to_image, cv2.IMREAD_COLOR)
+
+    pp_image = processing.PreProcessing(image, params['gamma'], params['contrast'], params['threshold']).result_image
+    lines_util = processing.LinesUtil(image, pp_image)
+
+    lines = lines_util.get_lines()
+
+    for line in lines:
+        # Name recognition
+        name = None
+        # todo
+
+        # Surname recognition
+        surname = None
+        # todo
+
+        index = rec_numbers.get_index(image, line)
+
+        result.append((name, surname, index))
+
+    return result
