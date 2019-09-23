0d150cff9c7e0c52083aa1b24675274e2a06dd9e
yaxlie <m.lesnierowski@gmail.com>
2019-09-24 01:42:01 +0200

    Add dir and file recognition

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ MODEL_FILENAME = 'rec_digits.h5'
 
 def main():
     if len(sys.argv) > 1:
-        result = ocr.ocr(str(sys.argv[1]))
-        print('\n'.join(map(str, result)))
-    else:
-        print("Nie podano ścieżki do pliku. Rozpoznawanie plików z katalogu /res")
-        model = keras.models.load_model('saved_models/' + MODEL_FILENAME)
+        if str(sys.argv[1]).endswith(('.jpg', '.png', '.gif')):
+            result = ocr.ocr(str(sys.argv[1]))
+            print('\n'.join(map(str, result)))
+        else:
+            model = keras.models.load_model('saved_models/' + MODEL_FILENAME)
 
-        for file in glob.iglob('res/**', recursive=True):
-            print(file)
-            if file.endswith('.jpg'):
-                result = ocr.ocr(file, model)
-                print('\n'.join(map(str, result)))
+            for file in glob.iglob('{}/**'.format(str(sys.argv[1])), recursive=True):
+                print(file)
+                if file.endswith('.jpg'):
+                    result = ocr.ocr(file, model)
+                    print('\n'.join(map(str, result)))
+    else:
+        print("Nie podano ścieżki do pliku lub katalogu.")
 
 
 if __name__ == '__main__':
